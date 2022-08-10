aca3733708eead6f56b2f12d68af864355ffc3b9
JahvinCrabtree <JahvinCrabtree@hotmail.com>
2022-08-10 01:54:20 +0100

    Fixed issue with the "UsedLetters" tidied up some playability problems

--- hangman.py
+++ hangman.py
@@ -4,13 +4,14 @@ from words import words
 
 def getValidWords(words):
     word = random.choice(words)
-    while '-' in word or ' ' in word:
+    while "-" in word or " " in word:
         word = random.choice(words)
     
     return word.upper()
 
+
 def hangman():
-    word = getValidWords
+    word = getValidWords(words)
     wordLetters = set(word) 
     alphabet = set(string.ascii_uppercase)
     usedLetters = set()
@@ -18,10 +19,13 @@ def hangman():
     lives = 6
 
     while len(wordLetters) > 0 and lives  > 0:
-        print ("You have used these letters: ', ' ".join(usedLetters))
+        print ("You have", lives, "lives left and you have used these letters: ", " ".join(usedLetters))
+
+        
 
         wordList = [letter if letter in usedLetters else "-" for letter in word]
-        print("Current word: ', ' ".join(wordList))
+        print("Current word: ", " " .join(wordList))
+   
 
         userLetter = input("Guess a letter: ").upper()
         if userLetter in alphabet - usedLetters:
@@ -31,7 +35,8 @@ def hangman():
 
             else:
                 lives = lives - 1
-                print("Letter is not in the word.")
+                print("\n The letter," , userLetter, "is not in the chosen word try again.")
+
 
         elif userLetter in usedLetters:
             print("Letter previously used, Try a different letter.")
@@ -44,5 +49,5 @@ def hangman():
         print ("Congratulations! You guessed the correct word", word, "!")
 
 
-userInput = input("Type Anything:")
-print(userInput)
+if __name__ == '__main__':
+    hangman()
